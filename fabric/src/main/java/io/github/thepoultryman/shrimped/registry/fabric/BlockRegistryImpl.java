5ee9ad494e09912d47be62a817d5c3ae418779ff
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-13 23:51:33 -0400

    feat(fabric): implement registry methods

--- fabric/src/main/java/io/github/thepoultryman/shrimped/registry/fabric/BlockRegistryImpl.java
+++ fabric/src/main/java/io/github/thepoultryman/shrimped/registry/fabric/BlockRegistryImpl.java
@@ -1,5 +1,11 @@
 package io.github.thepoultryman.shrimped.registry.fabric;
 
+import io.github.thepoultryman.shrimped.Shrimped;
+import io.github.thepoultryman.shrimped.registry.ItemRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
@@ -7,5 +13,7 @@ import java.util.function.Supplier;
 
 public class BlockRegistryImpl {
     public static void register(String id, Supplier<Block> blockSupplier, Item.Properties itemProperties) {
+        Block block = Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(Shrimped.MOD_ID, id), blockSupplier.get());
+        ItemRegistry.register(id, () -> new BlockItem(block, itemProperties));
     }
 }
--- fabric/src/main/java/io/github/thepoultryman/shrimped/registry/fabric/ItemRegistryImpl.java
+++ fabric/src/main/java/io/github/thepoultryman/shrimped/registry/fabric/ItemRegistryImpl.java
@@ -1,10 +1,15 @@
 package io.github.thepoultryman.shrimped.registry.fabric;
 
+import io.github.thepoultryman.shrimped.Shrimped;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Supplier;
 
 public class ItemRegistryImpl {
     public static <T extends Item> void register(String id, Supplier<T> itemSupplier) {
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Shrimped.MOD_ID, id), itemSupplier.get());
     }
 }
