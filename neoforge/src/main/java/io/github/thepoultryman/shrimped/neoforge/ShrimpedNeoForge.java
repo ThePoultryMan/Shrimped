d2b95901c16007eaed8c0e6bddae433a615fc9b5
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-13 22:51:21 -0400

    fix(neoforge): register differed registry

--- neoforge/src/main/java/io/github/thepoultryman/shrimped/neoforge/ShrimpedNeoForge.java
+++ neoforge/src/main/java/io/github/thepoultryman/shrimped/neoforge/ShrimpedNeoForge.java
@@ -1,11 +1,15 @@
 package io.github.thepoultryman.shrimped.neoforge;
 
 import io.github.thepoultryman.shrimped.Shrimped;
+import io.github.thepoultryman.shrimped.registry.neoforge.BlockRegistryImpl;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Shrimped.MOD_ID)
 public class ShrimpedNeoForge {
-    public ShrimpedNeoForge() {
+    public ShrimpedNeoForge(IEventBus eventBus) {
+        BlockRegistryImpl.BLOCK_REGISTRY.register(eventBus);
+
         Shrimped.init();
     }
 }
