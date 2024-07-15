0eebdf47a850a788fa7a5f66b7bdf2d95c18c3af
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-15 17:29:30 -0400

    fix(neoforge): register entity registry

--- neoforge/src/main/java/io/github/thepoultryman/shrimped/neoforge/ShrimpedNeoForge.java
+++ neoforge/src/main/java/io/github/thepoultryman/shrimped/neoforge/ShrimpedNeoForge.java
@@ -3,6 +3,7 @@ package io.github.thepoultryman.shrimped.neoforge;
 import io.github.thepoultryman.shrimped.Shrimped;
 import io.github.thepoultryman.shrimped.registry.neoforge.BlockRegistryImpl;
 import io.github.thepoultryman.shrimped.registry.neoforge.DataComponentRegistryImpl;
+import io.github.thepoultryman.shrimped.registry.neoforge.EntityRegistryImpl;
 import io.github.thepoultryman.shrimped.registry.neoforge.ItemRegistryImpl;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -13,6 +14,7 @@ public class ShrimpedNeoForge {
         BlockRegistryImpl.BLOCK_REGISTRY.register(eventBus);
         DataComponentRegistryImpl.DATA_COMPONENTS_REGISTRY.register(eventBus);
         ItemRegistryImpl.ITEM_REGISTRY.register(eventBus);
+        EntityRegistryImpl.ENTITY_TYPE_REGISTRY.register(eventBus);
 
         Shrimped.init();
     }
