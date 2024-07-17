a99cbebe8baa3f8e6d7bdfd720e1bcc110b4cc48
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-17 14:12:48 -0400

    chore(registry): remove unused parameter

--- common/src/main/java/io/github/thepoultryman/shrimped/registry/EntityRegistry.java
+++ common/src/main/java/io/github/thepoultryman/shrimped/registry/EntityRegistry.java
@@ -9,24 +9,20 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-
-import java.util.function.Supplier;
 
 public class EntityRegistry {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPE_REGISTER = DeferredRegister.create(Shrimped.MOD_ID, Registries.ENTITY_TYPE);
 
     public static final RegistrySupplier<EntityType<Shrimp>> SHRIMP_ENTITY_TYPE = register(
             "shrimp",
-            EntityType.Builder.of(Shrimp::new, MobCategory.WATER_CREATURE).sized(0.625f, 0.28125f).eyeHeight(0.09375f),
-            Shrimp::getShrimpAttributes);
+            EntityType.Builder.of(Shrimp::new, MobCategory.WATER_CREATURE).sized(0.625f, 0.28125f).eyeHeight(0.09375f));
 
     public static void registerEntities() {
         ENTITY_TYPE_REGISTER.register();
         EntityAttributeRegistry.register(SHRIMP_ENTITY_TYPE, Shrimp::getShrimpAttributes);
     }
 
-    public static <T extends LivingEntity> RegistrySupplier<EntityType<T>> register(String id, EntityType.Builder<T> builder, Supplier<AttributeSupplier.Builder> attributeBuilder) {
+    public static <T extends LivingEntity> RegistrySupplier<EntityType<T>> register(String id, EntityType.Builder<T> builder) {
         return ENTITY_TYPE_REGISTER.register(id, () -> builder.build(id));
     }
 }
